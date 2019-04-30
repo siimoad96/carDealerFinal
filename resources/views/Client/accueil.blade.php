f8e8679f459aac3f87eee3cd151a84ce7f40adff
siimoad <38928626+siimoad@users.noreply.github.com>
2019-05-01 00:40:49 +0100

    Fix app

--- resources/views/Client/accueil.blade.php
+++ resources/views/Client/accueil.blade.php
@@ -17,26 +17,12 @@
                 <div class="col-lg-4">
                 @foreach($annonces as $anno)
                 <h2>{{$anno->title}}</h2>
+                <br>
+                <h5>{{$anno->city}}</h5>
+                <h5>{{$anno->price}}</h5>
+                </div>
 @endforeach
-                <p>Info sur l'annonce  ................................................... etc.
-                        Les véhicules Tesla sont déclinés en 1 modèles depuis 2013. Utilisez le formulaire et accédez aux fiches techniques de votre modèle Tesla. 
-                </p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-                </div>
-                <div class="col-lg-4">
-                <h2>Annonce 2</h2>
-                <p>Info sur l'annonce  ................................................... etc.
-                        Les véhicules Tesla sont déclinés en 1 modèles depuis 2013. Utilisez le formulaire et accédez aux fiches techniques de votre modèle Tesla.  </p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-                <div class="col-lg-4">
-                <h2>Annonce 3</h2>
-                <p>Info sur l'annonce  ................................................... etc.
-                        Les véhicules Tesla sont déclinés en 1 modèles depuis 2013. Utilisez le formulaire et accédez aux fiches techniques de votre modèle Tesla. 
-                </p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-                </div>
-                @endforeach
+
                
             </div>
      
